be2109b8fe45aa688ca61599b152ca625dc8ca27
Marcio Sena <marciosena.inf@gmail.com>
2019-01-15 09:39:55 -0200

    fix conflict

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,24 +1,24 @@
 define({
-  "name": "bibleAPI",
-  "version": "0.2.0",
-  "description": "A RESTful API for Bible",
-  "title": "bibleAPI",
-  "url": "https://bibleapi.co/",
-  "header": {
-    "title": "bibleAPI",
-    "index": "Home",
-    "indexHref": "https://bibleapi.co"
+  name: 'bibleAPI',
+  version: '0.2.0',
+  description: 'A RESTful API for Bible',
+  title: 'bibleAPI',
+  url: 'https://bibleapi.co/',
+  header: {
+    title: 'bibleAPI',
+    index: 'Home',
+    indexHref: 'https://bibleapi.co'
   },
-  "template": {
-    "forceLanguage": "en"
+  template: {
+    forceLanguage: 'en'
   },
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2019-01-15T11:06:53.593Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.6"
+  sampleUrl: false,
+  defaultVersion: '0.0.0',
+  apidoc: '0.3.0',
+  generator: {
+    name: 'apidoc',
+    time: '2019-01-15T11:06:53.593Z',
+    url: 'http://apidocjs.com',
+    version: '0.17.6'
   }
-});
+})
