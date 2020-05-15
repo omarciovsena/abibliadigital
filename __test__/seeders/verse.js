1019ad49d00f2ec2e3a4a47d60a0c3ef76e36cad
Marcio Vinicius Sena <contato@marciosena.com.br>
2020-05-15 11:27:49 -0300

    fix: add logs and sync save

--- __test__/seeders/verse.js
+++ __test__/seeders/verse.js
@@ -16,16 +16,38 @@ mongoose.connection.on('error', err => {
   console.error(`%s ${err}`, chalk.red('x'))
 })
 
-const loadVerses = async (version) => {
-  const { data, lang, name } = version
+let total = 0
+const verses = []
+const lang = 'pt'
+
+function numberWithCommas (x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+const saveAll = () => {
+  const verse = verses.shift()
+  const doc = new Verse(verse)
+  doc.save((err, saved) => {
+    if (err) throw err
+    const nextVerse = verses[0]
+    if (nextVerse && verse.book.abbrev[lang] !== nextVerse.book.abbrev[lang]) {
+      console.log('   %s All verses off %s were saved. \n   -  %s verses to go.', chalk.green('✓'), chalk.blue(`${verse.book.abbrev[lang]}`), chalk.yellow(numberWithCommas(verses.length)))
+    }
+    --total ? saveAll() : process.exit()
+  })
+}
+
+const loadVerses = async (params) => {
+  const { data, name } = params
   const bookList = await Book.find()
   const booksMap = {}
+
   await bookList.map(book => (booksMap[book.abbrev[lang]] = book._id))
 
   try {
     for (const book of data) {
       await book.chapters.map(async (chapter, chapterIndex) => {
-        await chapter.map(async (verse, verseIndex) => {
+        return chapter.map(async (verse, verseIndex) => {
           const newVerse = {
             book: {
               id: booksMap[book.abbrev] && booksMap[book.abbrev]._id,
@@ -38,18 +60,22 @@ const loadVerses = async (version) => {
             text: verse,
             version: name
           }
-          await Verse.create(newVerse)
+          verses.push(newVerse)
         })
       })
     }
   } catch (err) {
-    console.log('Error', err)
+    console.log(` - %s error - ${err}`, chalk.red('x'))
+  } finally {
+    console.log(' - %s verses will be persisted.', chalk.yellow(numberWithCommas(verses.length)))
+    total = verses.length
+    saveAll()
   }
 }
 
+console.log('%s All verses will be deleted and registered again. \n %s There are more than %s verses, this can take a few minutes.', chalk.green('✓'), chalk.blue('-'), chalk.yellow('30,000'))
+
 Verse.deleteMany({}, async () => {
-  const versions = [
-    { data: nvi, name: 'nvi', lang: 'pt' }
-  ]
-  loadVerses(versions[1])
+  console.log(' %s All verses have been erased.', chalk.green('✓'))
+  loadVerses({ data: nvi, name: 'nvi' })
 })
