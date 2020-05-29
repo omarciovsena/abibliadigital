455e15bb69cc233863058aba202441e88a69de7e
Marcio Vinicius Sena <contato@marciosena.com.br>
2020-05-29 15:25:53 -0300

    chore: sort lines

--- __test__/helpers/index.test.js
+++ __test__/helpers/index.test.js
@@ -2,9 +2,9 @@ import { randomNumber } from '../../helpers/'
 
 jest.mock('axios')
 describe('helpers:randomNumber', () => {
-  it('must return a number less than 5', async () => {
+  it('must return a number less or equal than 5', async () => {
     const response = randomNumber(5)
-    expect(response < 5).toBeTruthy()
+    expect(response <= 5).toBeTruthy()
   })
 
   it('must be greater than 0', async () => {
